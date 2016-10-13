98461c2239f678ec7b2f0734f3be44573fe1d0fe
Osshrp <osshrp@gmail.com>
2016-10-13 14:52:55 +0300

    add spec and model post

---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    title "MyString"
+    body "MyString"
+    author "MyString"
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    nickname "MyString"
+    email "MyString"
+    password "MyString"
+  end
+end
