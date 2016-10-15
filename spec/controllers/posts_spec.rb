cff68178149a3ed2752972184a0353665742e4a9
Osshrp <osshrp@gmail.com>
2016-10-15 18:13:15 +0300

    add post PUT#update spec

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -3,14 +3,9 @@ require "rails_helper"
 module Api
   module V1
     describe PostsController, type: :request do
-      # before do
-      #   @user = User.create(
-      #                       nickname: 'Test',
-      #                       email: 'test@example.com',
-      #                       password: '123456'
-      #                      )
-      # end
+      let(:post_obj) { FactoryGirl.create(:post, user: user) }
       let!(:user) { FactoryGirl.create(:user) }
+
       describe "GET #index" do
         # let!(@user) { FactoryGirl.create(:user) }
         let!(:posts) { FactoryGirl.create_list(:post, 2, user: user) }
@@ -29,7 +24,7 @@ module Api
       end
 
       describe "POST #create" do
-        let!(:post_obj) { FactoryGirl.create(:post, user: user) }
+        # let!(:post_obj) { FactoryGirl.create(:post, user: user) }
         it "creates the new post" do
           post api_v1_posts_url,
             params: {
@@ -52,6 +47,24 @@ module Api
             headers: { "CONTENT_TYPE" => "application/vnd.api+json"}
           expect(response.content_type).to eq("application/vnd.api+json")
           expect(response.status).to eq(201)
+        end
+      end
+
+      describe "PUT #update" do
+        it "changes post title" do
+          patch api_v1_post_path(post_obj.id),
+            params:
+              {
+                "data": {
+                  "id": post_obj.id,
+                  "type": "posts",
+                  "attributes": {
+                    "title" => "new title",
+                  }
+                }
+              }.to_json,
+            headers: { "CONTENT_TYPE" => "application/vnd.api+json"}
+          expect(JSON.parse(response.body)["data"]["attributes"]["title"]).to eq "new title"
         end
       end
     end
