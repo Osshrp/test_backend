b63c8e5905c46edf389f2976d315597c0291c7a8
Osshrp <osshrp@gmail.com>
2016-10-25 08:14:51 +0300

    fix spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-feature "User connects Flickr photos to card" do
+feature "It shows profile page" do
   let!(:user) { FactoryGirl.create(:user) }
-  scenario "it has Load button" do
+  scenario "and has Load button" do
     visit "#{api_v1_users_path}/#{user.id}"
     expect(page).to have_content "Load"
   end
