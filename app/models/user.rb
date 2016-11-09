90ebb04c11d0e69ba7f79bc1b720cbb1732202b3
Osshrp <osshrp@gmail.com>
2016-11-09 10:42:44 +0300

    fix select_users query

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,33 @@ class User < ApplicationRecord
 
   validates :nickname, :email, :password, presence: true
   validates :avatar, file_size: { less_than_or_equal_to: 3.megabytes },
-                     file_content_type: { allow: ['image/jpeg',
-                                                  'image/jpg',
-                                                  'image/png'] }
+              file_content_type: { allow: ['image/jpeg',
+                                           'image/jpg',
+                                           'image/png'] }
   mount_uploader :avatar, AvatarUploader
 
   def self.select_users(params)
     users_arr = []
     users = User.left_joins(:posts, :comments).
-      where("posts.published_at between ? and ? or comments.published_at between ? and ?", "2016-10-19", "2016-10-20", "2016-10-19", "2016-10-19")
+      select("users.nickname, users.email,
+              count(posts.id) as posts_count,
+              count(comments.id) as comments_count").
+      where("posts.published_at between ? and ?
+             or comments.published_at between ? and ?",
+        params[:start_date].to_date, params[:end_date].to_date,
+        params[:start_date].to_date, params[:end_date].to_date).
+      group('users.nickname, users.email')
     users.each do |user|
       user_hash = {}
       user_hash = {
         nickname: user.nickname,
         email: user.email,
-        posts: user.posts.where("published_at between ? and ?",
-         params[:start_date].to_date, params[:end_date].to_date).count,
-        comments: user.comments.where("published_at between ? and ?",
-         params[:start_date], params[:end_date]).count
+        posts: user.posts_count,
+        comments: user.comments_count
       }
-      user_arrs << user_hash
+      users_arr << user_hash
     end
-    users.sort_by { |user| user[:quantity] = -(user[:posts]+user[:comments]).to_f/10 }
+    users_arr.sort_by { |user| user[:quantity] = -(user[:posts]+user[:comments]).to_f/10 }
   end
 
   private
